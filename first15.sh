92d55c00e2df118e38f016c168f9161f949434b8
Lovatoty <Lovatoty@gmail.com>
2020-11-19 12:55:40 -0600

    First Commit

--- first15.sh
+++ first15.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-whiptail --title "First 15 Script" --menu "Choose an Option" 32 60 16 \ 
-	"List Users" "Show all users with valid shells" \
-	"Firewall" "Configure Firewall Rules" \	
-	"<---" "Go Back"
-
+function usersWithShells(){
 whiptail --title "List of users with shells" --msgbox "The Following users have shells." 16 60 
 whiptail --title "List of users with shells" --textbox /dev/stdin 32 60 <<<"$(
 grep -v -e 'sync' -e 'bin/false' -e 'sbin/nologin' /etc/passwd)"
+}
 
-whiptail --msgbox "Time to set Firewall Rules" 16 60
-
+function firewallRules(){
 if (whiptail --yesno "Remove FirewallD and use IPTables?" 16 60); then
 	whiptail --textbox /dev/sddin 32 60 <<<"$(systectl disable --now )"
 	
@@ -19,5 +15,35 @@ if (whiptail --yesno "Remove FirewallD and use IPTables?" 16 60); then
 else
 	whiptail --msgbox "IPTables will not be configured." 16 60
 fi
-## Firewall rules go here
 
+}
+
+
+function menuFunction(){
+
+#using integer variable for portability
+EXITVAR=0
+
+while [ $EXITVAR -le 0 ]
+do
+
+      MENUSELECT=$(whiptail --title "First 15" --fb --menu "Where to Start" 15 60 4 \
+        "1" "View Users with Shells" \
+        "2" "Firewall Rules" \
+        "10" "Exit" 3>&1 1>&2 2>&3)
+    case $MENUSELECT in
+        1)
+		usersWithShells
+        ;;
+        2)
+		firewallRules
+        ;;
+        10)
+		whiptail --title "Exit" --msgbox "GoodBye!" 8 45
+		EXITVAR=5
+        ;;
+    esac
+done
+} 
+
+menuFunction
